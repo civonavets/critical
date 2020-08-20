66516b63453a2e18e72705a43adc7cb15541e90a
civonavets <28680216+civonavets@users.noreply.github.com>
2020-08-20 23:55:44 +0200

    Update critical.js

--- src/critical.js
+++ src/critical.js
@@ -1,20 +1,19 @@
-const webdriver = require('selenium-webdriver');
-const {Builder, By, Key, until} = require('selenium-webdriver');
-const data = require('../data/data.js');
-
+const webdriver = require("selenium-webdriver");
+const { Builder, By, Key, until } = require("selenium-webdriver");
+const data = require("../data/data.js");
+const page = data.url.concat(data.uri);
 async function helloThereGeneralKenobi() {
-    const driver = new webdriver.Builder().forBrowser('chrome').build();
-    try {
-        await driver.get(data.url);
-        await driver.wait(until.elementsLocated({css: data.le}), 2000);
-        await driver.findElement(By.css(data.fsc)).click();
-        await driver.sleep(2000);
-    }
-    finally {
-        setTimeout(function () {
-            console.log(data.cheers);
-            16500},
-        );
-    }
+  const driver = new webdriver.Builder().forBrowser("chrome").build();
+  try {
+    await driver.get(page);
+    await driver.wait(until.elementsLocated({ css: data.le }), 2000);
+    await driver.findElement(By.css(data.fsc)).click();
+    await driver.sleep(2000);
+  } finally {
+    setTimeout(function () {
+      console.log(data.cheers);
+      16500;
+    });
+  }
 }
 helloThereGeneralKenobi();
